6fffb18b46a4f0904c6af87f57fafb104af607e7
rogeswapnil <rogeswapnil@users.noreply.github.com>
2017-02-05 14:45:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
+
 // Change the text of main-text div
-var element = document.getElementById('main-text'
-    );
+var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+// Move the image
+var img = document.getElementById('img');
+img.onclick = funtion() {
+    img.style.marginleft = '100px';
+};
